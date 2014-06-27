d878383b0328cc8b53a89173bccfc45be421dc3d
Federico Mendez <federicomh@gmail.com>
2014-06-27 11:26:40 +0200

    Suggest service use separate response objects Facets based on Country and Language use the iso2LetterCode
    http://dev.gbif.org/issues/browse/POR-2206
    http://dev.gbif.org/issues/browse/POR-1481
    http://dev.gbif.org/issues/browse/POR-2232
    http://dev.gbif.org/code/cru/CR-POR-169

--- src/main/java/org/gbif/ws/client/BaseWsSuggestClient.java
+++ src/main/java/org/gbif/ws/client/BaseWsSuggestClient.java
@@ -15,31 +15,29 @@ import com.sun.jersey.api.client.WebResource;
 /**
  * Base web service suggest & faceted search client.
  */
-public abstract class BaseWsSuggestClient<T, P extends Enum<?> & SearchParameter,
-  R extends FacetedSearchRequest<P>, RSUG extends SearchRequest<P>>
-  extends BaseWsFacetedSearchClient<T, P, R> implements SuggestService<T, P, RSUG> {
+public abstract class BaseWsSuggestClient<T, P extends Enum<?> & SearchParameter, R extends FacetedSearchRequest<P>, ST, RSUG extends SearchRequest<P>>
+  extends BaseWsFacetedSearchClient<T, P, R> implements SuggestService<ST, P, RSUG> {
 
-  private final GenericType<List<T>> suggestType;
+  private final GenericType<List<ST>> suggestType;
 
   /**
-   * @param resource    the base web resource being the parent of /search and /suggest
-   * @param searchType  the generic type for the search response.
+   * @param resource the base web resource being the parent of /search and /suggest
+   * @param searchType the generic type for the search response.
    * @param suggestType the generic type for the suggest response.
    */
   protected BaseWsSuggestClient(WebResource resource, GenericType<SearchResponse<T, P>> searchType,
-    GenericType<List<T>> suggestType) {
+    GenericType<List<ST>> suggestType) {
     super(resource, searchType);
     this.suggestType = suggestType;
   }
 
   /**
    * Generic suggest operation retrieving search entities T.
-   *
+   * 
    * @param suggestRequest to issue the search/suggest operation.
-   *
    * @return a List of NameUsageSearchSuggestResult objects
    */
-  public List<T> suggest(RSUG suggestRequest) {
+  public List<ST> suggest(RSUG suggestRequest) {
     if (suggestRequest == null) {
       throw new IllegalArgumentException("searchSuggestRequest cannot be null");
     }
--- src/main/java/org/gbif/ws/server/provider/NameUsageSuggestRequestProvider.java
+++ src/main/java/org/gbif/ws/server/provider/NameUsageSuggestRequestProvider.java
@@ -1,12 +1,9 @@
 /*
  * Copyright 2011 Global Biodiversity Information Facility (GBIF)
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,6 +16,7 @@ import org.gbif.api.model.checklistbank.search.NameUsageSearchParameter;
 import org.gbif.api.model.checklistbank.search.NameUsageSuggestRequest;
 
 import java.lang.reflect.Type;
+
 import javax.ws.rs.core.Context;
 import javax.ws.rs.ext.Provider;
 
