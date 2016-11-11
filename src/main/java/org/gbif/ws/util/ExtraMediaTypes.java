b2de7430bc0af203579d411eedcc3c045f9bbbbc
Christian Gendreau <cgendreau@gbif.org>
2016-11-11 16:18:57 +0100

    more Extra mime types

--- src/main/java/org/gbif/ws/util/ExtraMediaTypes.java
+++ src/main/java/org/gbif/ws/util/ExtraMediaTypes.java
@@ -31,6 +31,18 @@ public class ExtraMediaTypes {
   public static final String TEXT_TSV = "text/tab-separated-values";
   public static final MediaType TEXT_TSV_TYPE = new MediaType("text", "tab-separated-values");
 
+  //.xls
+  public static final String APPLICATION_EXCEL = "application/vnd.ms-excel";
+  public static final MediaType APPLICATION_EXCEL_TYPE = new MediaType("application", "vnd.ms-excel");
+
+  //.xlsx
+  public static final String APPLICATION_OFFICE_SPREADSHEET = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+  public static final MediaType APPLICATION_OFFICE_SPREADSHEET_TYPE = new MediaType("application", "vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+
+  //.ods
+  public static final String APPLICATION_OPEN_DOC_SPREADSHEET = "application/vnd.oasis.opendocument.spreadsheet";
+  public static final MediaType APPLICATION_OPEN_DOC_SPREADSHEET_TYPE = new MediaType("application", "vnd.oasis.opendocument.spreadsheet");
+
 
   /**
    * Darwin Core archive media type with underlying zip structure.
