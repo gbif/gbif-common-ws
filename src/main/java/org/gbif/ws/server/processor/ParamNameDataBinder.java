8355ef3fccd2ca0e590d36961e92e3467cbd2678
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2020-02-10 16:42:00 +0100

    ParamName annotation and processor

---
+++ src/main/java/org/gbif/ws/server/processor/ParamNameDataBinder.java
@@ -0,0 +1,62 @@
+package org.gbif.ws.server.processor;
+
+import java.lang.reflect.Method;
+import java.util.Map;
+import javax.servlet.ServletRequest;
+import org.gbif.ws.annotation.ParamName;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.MutablePropertyValues;
+import org.springframework.web.servlet.mvc.method.annotation.ExtendedServletRequestDataBinder;
+
+/** ServletRequestDataBinder which supports fields renaming using {@link ParamName} */
+public class ParamNameDataBinder extends ExtendedServletRequestDataBinder {
+
+  private static final Logger LOG = LoggerFactory.getLogger(ParamNameDataBinder.class);
+
+  private final Map<String, String> paramMappings;
+  private final Map<String, String> methodMappings;
+
+  public ParamNameDataBinder(
+      Object target,
+      String objectName,
+      Map<String, String> paramMappings,
+      Map<String, String> methodMappings) {
+    super(target, objectName);
+    this.paramMappings = paramMappings;
+    this.methodMappings = methodMappings;
+  }
+
+  /**
+   * Using a mapping which was created by {@link ParamNameProcessor} this method binds actual values
+   * to the parameters.
+   */
+  @Override
+  protected void addBindValues(
+      MutablePropertyValues mutablePropertyValues, ServletRequest request) {
+    super.addBindValues(mutablePropertyValues, request);
+    for (Map.Entry<String, String> entry : paramMappings.entrySet()) {
+      String paramName = entry.getKey();
+      String fieldName = entry.getValue();
+      if (mutablePropertyValues.contains(paramName)) {
+        mutablePropertyValues.add(
+            fieldName, mutablePropertyValues.getPropertyValue(paramName).getValue());
+      }
+    }
+
+    for (Map.Entry<String, String> entry : methodMappings.entrySet()) {
+      String paramName = entry.getKey();
+      String methodName = entry.getValue();
+      if (mutablePropertyValues.contains(paramName)) {
+        try {
+          final Method declaredMethod =
+              getTarget().getClass().getDeclaredMethod(methodName, String.class);
+          declaredMethod.invoke(
+              getTarget(), mutablePropertyValues.getPropertyValue(paramName).getValue());
+        } catch (Exception e) {
+          LOG.error("There was a problem to invoke a method {}", methodName);
+        }
+      }
+    }
+  }
+}
---
+++ src/main/java/org/gbif/ws/server/processor/ParamNameProcessor.java
@@ -0,0 +1,105 @@
+package org.gbif.ws.server.processor;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import org.gbif.ws.annotation.ParamName;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+import org.springframework.web.servlet.mvc.method.annotation.ServletModelAttributeMethodProcessor;
+
+/** Process {@link ParamName}. */
+public class ParamNameProcessor extends ServletModelAttributeMethodProcessor {
+
+  @Autowired private RequestMappingHandlerAdapter requestMappingHandlerAdapter;
+
+  private static final Map<Class<?>, Map<String, String>> PARAM_MAPPINGS_CACHE =
+      new ConcurrentHashMap<>(256);
+  private static final Map<Class<?>, Map<String, String>> METHODS_MAPPINGS_CACHE =
+      new ConcurrentHashMap<>(256);
+
+  public ParamNameProcessor() {
+    super(false);
+  }
+
+  @Override
+  public boolean supportsParameter(MethodParameter parameter) {
+    return parameter.hasParameterAnnotation(RequestParam.class)
+        && !BeanUtils.isSimpleProperty(parameter.getParameterType())
+        && (Arrays.stream(parameter.getParameterType().getDeclaredMethods())
+                .anyMatch(method -> method.getAnnotation(ParamName.class) != null)
+            || Arrays.stream(parameter.getParameterType().getDeclaredFields())
+                .anyMatch(field -> field.getAnnotation(ParamName.class) != null));
+  }
+
+  @Override
+  protected void bindRequestParameters(WebDataBinder binder, NativeWebRequest nativeWebRequest) {
+    Object target = binder.getTarget();
+    Map<String, String> paramMappings = this.getParamMappings(target.getClass());
+    Map<String, String> methodMappings = this.getMethodMappings(target.getClass());
+    ParamNameDataBinder paramNameDataBinder =
+        new ParamNameDataBinder(target, binder.getObjectName(), paramMappings, methodMappings);
+    requestMappingHandlerAdapter.getWebBindingInitializer().initBinder(paramNameDataBinder);
+    super.bindRequestParameters(paramNameDataBinder, nativeWebRequest);
+  }
+
+  /**
+   * Get param mappings. It creates a simple mapping: parameter name -> field name. Cache param
+   * mappings in memory.
+   */
+  private Map<String, String> getParamMappings(Class<?> targetClass) {
+    // first check cache
+    if (PARAM_MAPPINGS_CACHE.containsKey(targetClass)) {
+      return PARAM_MAPPINGS_CACHE.get(targetClass);
+    }
+
+    Map<String, String> paramMappings = new HashMap<>(32);
+
+    // process fields
+    Field[] fields = targetClass.getDeclaredFields();
+    for (Field field : fields) {
+      ParamName paramName = field.getAnnotation(ParamName.class);
+      if (paramName != null && !paramName.value().isEmpty()) {
+        paramMappings.put(paramName.value(), field.getName());
+      }
+    }
+
+    // put them to cache
+    PARAM_MAPPINGS_CACHE.put(targetClass, paramMappings);
+    return paramMappings;
+  }
+
+  /**
+   * Get param mappings. It creates a simple mapping: parameter name -> method name. Cache param
+   * mappings in memory.
+   */
+  private Map<String, String> getMethodMappings(Class<?> targetClass) {
+    // first check cache
+    if (METHODS_MAPPINGS_CACHE.containsKey(targetClass)) {
+      return METHODS_MAPPINGS_CACHE.get(targetClass);
+    }
+
+    Map<String, String> methodMappings = new HashMap<>(32);
+
+    // process methods
+    final Method[] methods = targetClass.getDeclaredMethods();
+    for (Method method : methods) {
+      final ParamName paramName = method.getAnnotation(ParamName.class);
+      if (paramName != null && !paramName.value().isEmpty()) {
+        methodMappings.put(paramName.value(), method.getName());
+      }
+    }
+
+    // put them to cache
+    METHODS_MAPPINGS_CACHE.put(targetClass, methodMappings);
+    return methodMappings;
+  }
+}
