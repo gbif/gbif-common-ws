8c43ac7b831f58feabe5e018b50278ef3fa9cf0c
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2020-03-03 17:40:02 +0100

    NullToNotFoundAspect improvements

--- src/main/java/org/gbif/ws/server/aspect/NullToNotFoundAspect.java
+++ src/main/java/org/gbif/ws/server/aspect/NullToNotFoundAspect.java
@@ -1,9 +1,14 @@
 package org.gbif.ws.server.aspect;
 
+import java.net.URI;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.gbif.api.annotation.NullToNotFound;
 import org.gbif.ws.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 /**
@@ -13,11 +18,30 @@ import org.springframework.stereotype.Component;
 @Aspect
 public class NullToNotFoundAspect {
 
+  private static final Logger LOG = LoggerFactory.getLogger(NullToNotFoundAspect.class);
+
   @AfterReturning(pointcut = "@annotation(org.gbif.api.annotation.NullToNotFound)",
       returning = "retVal")
   public void afterReturningAdvice(JoinPoint jp, Object retVal) {
     if (retVal == null) {
-      throw new NotFoundException();
+      String uri = ((MethodSignature) jp.getSignature()).getMethod()
+          .getAnnotation(NullToNotFound.class).value();
+
+      String[] parameterNames = ((MethodSignature) jp.getSignature()).getParameterNames();
+      Object[] parameterValues = jp.getArgs();
+
+      // replace pat variables in URI with values
+      for (int i = 0; i < parameterNames.length; i++) {
+        if (uri.contains('{' + parameterNames[i] + '}')) {
+          uri = uri.replace('{' + parameterNames[i] + '}', parameterValues[i].toString());
+        }
+      }
+
+      if (uri.contains("{") || uri.contains("}")) {
+        LOG.warn("URI was not processed properly and contains special characters {}", uri);
+      }
+
+      throw new NotFoundException("Entity not found", URI.create(uri));
     }
   }
 }
