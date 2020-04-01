ec3863bfee660443a7cfcd74136178e4e0c1559e
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2020-04-01 14:53:48 +0200

    Add ClientFactory

---
+++ src/main/java/org/gbif/ws/client/ClientFactory.java
@@ -0,0 +1,52 @@
+package org.gbif.ws.client;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import feign.Contract;
+import feign.Feign;
+import feign.codec.Decoder;
+import feign.codec.ErrorDecoder;
+import feign.jackson.JacksonDecoder;
+import org.gbif.ws.json.JacksonJsonObjectMapperProvider;
+import org.gbif.ws.security.Md5EncodeService;
+import org.gbif.ws.security.Md5EncodeServiceImpl;
+import org.gbif.ws.security.SecretKeySigningService;
+import org.gbif.ws.security.SigningService;
+
+public class ClientFactory {
+
+  private String url;
+  private GbifAuthRequestInterceptor requestInterceptor;
+  private Decoder decoder;
+  private ErrorDecoder errorDecoder;
+  private Contract contract;
+
+  public ClientFactory(String url, String appKey, String secretKey) {
+    this.url = url;
+    ObjectMapper objectMapper = JacksonJsonObjectMapperProvider.getObjectMapper();
+    this.requestInterceptor =
+        new GbifAuthRequestInterceptor(appKey, secretKey, new SecretKeySigningService(),
+            new Md5EncodeServiceImpl(objectMapper));
+    this.decoder = new JacksonDecoder(objectMapper);
+    this.errorDecoder = new ClientErrorDecoder();
+    this.contract = new ClientContract();
+  }
+
+  public ClientFactory(String url, String appKey, String secretKey, SigningService signingService,
+      Md5EncodeService md5EncodeService, ObjectMapper objectMapper) {
+    this.url = url;
+    this.requestInterceptor =
+        new GbifAuthRequestInterceptor(appKey, secretKey, signingService, md5EncodeService);
+    this.decoder = new JacksonDecoder(objectMapper);
+    this.errorDecoder = new ClientErrorDecoder();
+    this.contract = new ClientContract();
+  }
+
+  public <T> T newInstance(Class<T> clazz) {
+    return Feign.builder()
+        .decoder(decoder)
+        .errorDecoder(errorDecoder)
+        .contract(contract)
+        .requestInterceptor(requestInterceptor)
+        .target(clazz, url);
+  }
+}
