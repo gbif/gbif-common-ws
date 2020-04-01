59583df52910b59d3148cc3c41d13ff4d50cd9c2
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2020-04-01 15:33:00 +0200

    Add Client jackson encoder

--- src/main/java/org/gbif/ws/client/ClientFactory.java
+++ src/main/java/org/gbif/ws/client/ClientFactory.java
@@ -4,8 +4,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import feign.Contract;
 import feign.Feign;
 import feign.codec.Decoder;
+import feign.codec.Encoder;
 import feign.codec.ErrorDecoder;
 import feign.jackson.JacksonDecoder;
+import feign.jackson.JacksonEncoder;
 import org.gbif.ws.json.JacksonJsonObjectMapperProvider;
 import org.gbif.ws.security.Md5EncodeService;
 import org.gbif.ws.security.Md5EncodeServiceImpl;
@@ -17,6 +19,7 @@ public class ClientFactory {
   private String url;
   private GbifAuthRequestInterceptor requestInterceptor;
   private Decoder decoder;
+  private Encoder encoder;
   private ErrorDecoder errorDecoder;
   private Contract contract;
 
@@ -26,6 +29,7 @@ public class ClientFactory {
     this.requestInterceptor =
         new GbifAuthRequestInterceptor(appKey, secretKey, new SecretKeySigningService(),
             new Md5EncodeServiceImpl(objectMapper));
+    this.encoder = new JacksonEncoder(objectMapper);
     this.decoder = new JacksonDecoder(objectMapper);
     this.errorDecoder = new ClientErrorDecoder();
     this.contract = new ClientContract();
@@ -36,6 +40,7 @@ public class ClientFactory {
     this.url = url;
     this.requestInterceptor =
         new GbifAuthRequestInterceptor(appKey, secretKey, signingService, md5EncodeService);
+    this.encoder = new JacksonEncoder(objectMapper);
     this.decoder = new JacksonDecoder(objectMapper);
     this.errorDecoder = new ClientErrorDecoder();
     this.contract = new ClientContract();
@@ -43,6 +48,7 @@ public class ClientFactory {
 
   public <T> T newInstance(Class<T> clazz) {
     return Feign.builder()
+        .encoder(encoder)
         .decoder(decoder)
         .errorDecoder(errorDecoder)
         .contract(contract)
