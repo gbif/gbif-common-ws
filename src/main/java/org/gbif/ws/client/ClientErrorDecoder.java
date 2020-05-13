7e72b2b43bbc346fc2215acaf2660c6a81e6190d
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2020-05-13 23:21:08 +0200

    Improve ClientErrorDecoder

--- src/main/java/org/gbif/ws/client/ClientErrorDecoder.java
+++ src/main/java/org/gbif/ws/client/ClientErrorDecoder.java
@@ -1,18 +1,36 @@
 package org.gbif.ws.client;
 
+import com.google.common.io.CharStreams;
 import feign.Response;
 import feign.codec.ErrorDecoder;
 import org.gbif.api.exception.ServiceUnavailableException;
 import org.gbif.ws.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.access.AccessDeniedException;
 
 import javax.validation.ValidationException;
+import java.io.IOException;
+import java.io.Reader;
 import java.net.URI;
 
+@SuppressWarnings("UnstableApiUsage")
 public class ClientErrorDecoder implements ErrorDecoder {
+
+  private static final Logger LOG = LoggerFactory.getLogger(ClientErrorDecoder.class);
 
   @Override
   public Exception decode(String methodKey, Response response) {
+    String message = null;
+
+    try (Reader reader = response.body().asReader()) {
+      //Easy way to read the stream and get a String object
+      message = CharStreams.toString(reader);
+      LOG.error("Client exception: {}", message);
+    } catch (IOException e) {
+      LOG.error("Exception during reading client error response", e);
+    }
+
     switch (response.status()) {
       case 400:
         return new IllegalArgumentException("A bad request received");
@@ -23,13 +41,21 @@ public class ClientErrorDecoder implements ErrorDecoder {
       case 404:
         return new NotFoundException("Resource not found", URI.create(response.request().url()));
       case 422:
-        return new ValidationException();
+        return message != null
+            ? new ValidationException(extractValidationErrorMessage(message))
+            : new ValidationException();
       case 500:
         return new ServiceUnavailableException("An internal server error occurred, please try again later");
       case 501:
-        return new UnsupportedOperationException("Method not implement yet");
+        return new UnsupportedOperationException(message != null ? message : "Method not implement yet");
       default:
-        return new RuntimeException("Unexpected exception");
+        return new RuntimeException(message != null ? message : "Unexpected exception");
     }
+  }
+
+  private String extractValidationErrorMessage(String message) {
+    return message.replace("<ul><li>", "")
+        .replace("</li></ul>", "")
+        .replace("<li></li>", ", ");
   }
 }
