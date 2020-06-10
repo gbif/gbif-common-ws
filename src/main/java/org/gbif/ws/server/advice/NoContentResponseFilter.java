c58634608e0882d6a22c3b0ee80ef5769f13e686
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2020-06-10 16:49:50 +0200

    Add NoContentResponseFilter

--- src/main/java/org/gbif/ws/server/advice/NoContentResponseFilter.java
+++ src/main/java/org/gbif/ws/server/advice/NoContentResponseFilter.java
@@ -1,29 +1,20 @@
 package org.gbif.ws.server.advice;
 
 import org.springframework.core.MethodParameter;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
-import org.springframework.http.server.ServletServerHttpResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
 /**
- * Filter that updates http headers when a new resource is successfully created via a POST request unless
- * the response returns 204 No Content.
- * <p>
- * The following headers are added or replaced if they existed:
- * <ul>
- *   <li>Http response code 201</li>
- *   <li>Location header is set accordingly based on returned key</li>
- * </ul>
+ * Response filter that converts response status to 204 NO_CONTENT if response body is null.
  */
 @SuppressWarnings("NullableProblems")
 @ControllerAdvice
-public class CreatedResponseFilter implements ResponseBodyAdvice<Object> {
+public class NoContentResponseFilter implements ResponseBodyAdvice<Object> {
 
   @Override
   public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
@@ -37,14 +28,8 @@ public class CreatedResponseFilter implements ResponseBodyAdvice<Object> {
                                 Class<? extends HttpMessageConverter<?>> selectedConverterType,
                                 ServerHttpRequest request,
                                 ServerHttpResponse response) {
-    final int intStatus = ((ServletServerHttpResponse) response).getServletResponse().getStatus();
-    final HttpStatus httpStatus = HttpStatus.resolve(intStatus);
-
-    if (request.getMethod() != null && httpStatus != null && request.getMethod() == HttpMethod.POST
-        && httpStatus != HttpStatus.NO_CONTENT && httpStatus.is2xxSuccessful()) {
-      response.setStatusCode(HttpStatus.CREATED);
-
-      // TODO: 15/01/2020 if response contains the key, also set Location
+    if (body == null) {
+      response.setStatusCode(HttpStatus.NO_CONTENT);
     }
 
     return body;
