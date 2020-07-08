6dd2437ba3e030ad20d882b6d052e8ad1c531e1b
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2020-07-08 12:54:08 +0200

    Refactor ClientDecoder

--- src/main/java/org/gbif/ws/client/ClientDecoder.java
+++ src/main/java/org/gbif/ws/client/ClientDecoder.java
@@ -10,7 +10,6 @@ import feign.jackson.JacksonDecoder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.util.StreamUtils;
 import org.springframework.util.StringUtils;
 
 import java.io.IOException;
@@ -18,7 +17,7 @@ import java.lang.reflect.Type;
 
 public class ClientDecoder implements Decoder {
 
-  private JacksonDecoder jacksonDecoder;
+  private final JacksonDecoder jacksonDecoder;
 
   public ClientDecoder(ObjectMapper objectMapper) {
     this.jacksonDecoder = new JacksonDecoder(objectMapper);
@@ -26,26 +25,26 @@ public class ClientDecoder implements Decoder {
 
   @Override
   public Object decode(Response response, Type type)
-      throws IOException, DecodeException, FeignException {
+      throws IOException, FeignException {
     HttpStatus responseStatus = HttpStatus.resolve(response.status());
 
-    if (HttpStatus.NOT_FOUND  == responseStatus) {
+    if (HttpStatus.NOT_FOUND == responseStatus) {
       return null;
-    } else if (responseStatus.isError()) {
+    } else if (responseStatus != null && responseStatus.isError()) {
       throw new DecodeException(response.status(), response.toString(), response.request());
     }
 
     MediaType contentType = getContentType(response);
     if (MediaType.APPLICATION_JSON.equalsTypeAndSubtype(contentType)) {
       return jacksonDecoder.decode(response, type);
-    } else if (MediaType.TEXT_PLAIN.equalsTypeAndSubtype(contentType)){
-      return  Util.toString(response.body().asReader());
+    } else if (MediaType.TEXT_PLAIN.equalsTypeAndSubtype(contentType)) {
+      return Util.toString(response.body().asReader());
     } else if (MediaType.APPLICATION_OCTET_STREAM.equalsTypeAndSubtype(contentType)) {
-      return StreamUtils.copyToByteArray(response.body().asInputStream());
+      return Util.toByteArray(response.body().asInputStream());
     } else if (byte[].class.equals(type)) {
       return Util.toByteArray(response.body().asInputStream());
     } else {
-      throw  new DecodeException(response.status(), "Unsupported response type", response.request());
+      throw new DecodeException(response.status(), "Unsupported response type", response.request());
     }
   }
 
@@ -54,8 +53,8 @@ public class ClientDecoder implements Decoder {
    */
   private static MediaType getContentType(Response response) {
     return response.headers().get(HttpHeaders.CONTENT_TYPE).stream()
-                              .findFirst()
-                              .filter(StringUtils::hasLength)
-                              .map(MediaType::parseMediaType).orElse(null);
+        .findFirst()
+        .filter(StringUtils::hasLength)
+        .map(MediaType::parseMediaType).orElse(null);
   }
 }
