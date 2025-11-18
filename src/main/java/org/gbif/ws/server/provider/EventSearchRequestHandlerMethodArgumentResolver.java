2bfc5775063e9a73cfb4646a3d7fd7422f17d8ae
Marcos Lopez Gonzalez <marcoslg@gmail.com>
2025-11-18 11:55:34 +0100

    events refactor (#25)
    * events refactor
    
    * PR feedback: refactor event search request handler and parameter utilities
    
    * update version numbers in pom.xml to remove event-specific suffixes

--- src/main/java/org/gbif/ws/server/provider/EventSearchRequestHandlerMethodArgumentResolver.java
+++ src/main/java/org/gbif/ws/server/provider/EventSearchRequestHandlerMethodArgumentResolver.java
@@ -13,12 +13,11 @@
  */
 package org.gbif.ws.server.provider;
 
-import static org.gbif.ws.util.ParamUtils.convertDnaSequenceParam;
 import static org.gbif.ws.util.ParamUtils.convertHumboldtUnitsParam;
 
 import java.util.Optional;
-import org.gbif.api.model.occurrence.search.OccurrenceSearchParameter;
-import org.gbif.api.model.occurrence.search.OccurrenceSearchRequest;
+import org.gbif.api.model.event.search.EventSearchParameter;
+import org.gbif.api.model.event.search.EventSearchRequest;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
@@ -26,20 +25,20 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
-public class OccurrenceSearchRequestHandlerMethodArgumentResolver
-    extends FacetedSearchRequestProvider<OccurrenceSearchRequest, OccurrenceSearchParameter>
+public class EventSearchRequestHandlerMethodArgumentResolver
+    extends FacetedSearchRequestProvider<EventSearchRequest, EventSearchParameter>
     implements HandlerMethodArgumentResolver {
 
   private static final String MATCH_CASE_PARAM = "matchCase";
   private static final String SHUFFLE_PARAM = "shuffle";
 
-  public OccurrenceSearchRequestHandlerMethodArgumentResolver() {
-    super(OccurrenceSearchRequest.class, OccurrenceSearchParameter.class);
+  public EventSearchRequestHandlerMethodArgumentResolver() {
+    super(EventSearchRequest.class, EventSearchParameter.class);
   }
 
   @Override
   public boolean supportsParameter(MethodParameter parameter) {
-    return OccurrenceSearchRequest.class.equals(parameter.getParameterType());
+    return EventSearchRequest.class.equals(parameter.getParameterType());
   }
 
   @Override
@@ -52,20 +51,20 @@ public class OccurrenceSearchRequestHandlerMethodArgumentResolver
   }
 
   @Override
-  protected OccurrenceSearchRequest getSearchRequest(
-      WebRequest webRequest, OccurrenceSearchRequest searchRequest) {
-    OccurrenceSearchRequest occurrenceSearchRequest =
+  protected EventSearchRequest getSearchRequest(
+      WebRequest webRequest, EventSearchRequest searchRequest) {
+    EventSearchRequest eventSearchRequest =
         super.getSearchRequest(webRequest, searchRequest);
     Optional.ofNullable(webRequest.getParameter(MATCH_CASE_PARAM))
         .ifPresent(
             matchVerbatim ->
-                occurrenceSearchRequest.setMatchCase(Boolean.parseBoolean(matchVerbatim)));
+              eventSearchRequest.setMatchCase(Boolean.parseBoolean(matchVerbatim)));
 
     Optional.ofNullable(webRequest.getParameter(SHUFFLE_PARAM))
-        .ifPresent(occurrenceSearchRequest::setShuffle);
+        .ifPresent(eventSearchRequest::setShuffle);
 
-    convertDnaSequenceParam(webRequest.getParameterMap(), searchRequest);
+    convertHumboldtUnitsParam(webRequest.getParameterMap(), searchRequest);
 
-    return occurrenceSearchRequest;
+    return eventSearchRequest;
   }
 }
